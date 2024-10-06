08c2ca9a957812e5d66d94a8d34c15c8a765f93f
Leonardo Adamoli <leo.adamoli@gmail.com>
2024-10-06 19:11:46 -0300

    feat: adding hbs support and initial view file

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 // DEPENDENCIES
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { join } from 'path';
+
+// TYPES
+import type { NestExpressApplication } from '@nestjs/platform-express';
 
 // MODULES
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
   await app.listen(3000);
 }
 
